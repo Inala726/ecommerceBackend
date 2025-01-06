1c973b8886c9109e92441d5e2c21c5472109a92a
Inala <inalajacob740@gmail.com>
2025-01-06 05:01:32 +0100

    implemented user services

--- src/service/product.service.ts
+++ src/service/product.service.ts
@@ -1,2 +1,8 @@
 import { createProductDTO } from "../dtos/product.dto";
 import { Product } from "@prisma/client";
+
+export interface productServices{
+    addProduct(data: createProductDTO): Promise<Product>
+    deleteProduct(id: number): Promise<Product | null>
+    getAllProducts(): Promise<Product[]>
+}
